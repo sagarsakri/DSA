119f9759b6de6a0eaff08a713216fa6201935300
Abhiram0402 <Abhiramma2022@gmail.com>
2023-08-03 22:34:01 +0530

    print array

---
+++ src/main/java/arrays/PrintArray.java
@@ -0,0 +1,9 @@
+package arrays;
+
+public class PrintArray {
+    public void print(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+        }
+    }
+}
